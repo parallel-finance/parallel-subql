8f344eaa719157516e797fdce3d0eae7ef600d76
Ayush <>
2021-08-13 13:45:49 +0530

    corrected token decimal

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -2,9 +2,9 @@ import { Codec } from '@polkadot/types/types'
 
 export const TOKEN_DECIMAL = {
   DOT: 10,
-  USDT: 10,
-  xDOT: 8,
-  xKSM: 8,
+  USDT: 6,
+  xDOT: 12,
+  xKSM: 12,
   PARA: 12,
   KSM: 12,
   HKO: 12
