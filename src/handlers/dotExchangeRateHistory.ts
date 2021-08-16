1b0ff12a798fe0806ac7b8e3b43aa4acfce643d7
Ayush <>
2021-08-16 12:32:31 +0530

    Removed parallel-finance/types to fix build error

--- src/handlers/dotExchangeRateHistory.ts
+++ src/handlers/dotExchangeRateHistory.ts
@@ -2,7 +2,6 @@ import { SubstrateBlock, SubstrateEvent } from "@subql/types";
 import { BlockNumber } from "@polkadot/types/interfaces";
 import { Compact } from '@polkadot/types';
 import { DotExchangeRate } from "../types/models/DotExchangeRate";
-import { Rate } from "@parallel-finance/types/interfaces";
 
 export async function createDotExchangeRateHistory(block: SubstrateBlock): Promise<void> {
     const blockNumber = (block.block.header.number as Compact<BlockNumber>).toBigInt();
@@ -12,7 +11,7 @@ export async function createDotExchangeRateHistory(block: SubstrateBlock): Promi
 
     record.blockNumber = blockNumber;
 
-    const dotExchangeRate = (await api.query.liquidStaking.exchangeRate) as Rate;
+    const dotExchangeRate = (await api.query.liquidStaking.exchangeRate);
 
     record.rate = dotExchangeRate.toString();
 
