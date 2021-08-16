582e30a9feb6d19ace42877e6a4fe3dce645216a
Ayush <>
2021-08-16 19:28:06 +0530

    Added new handler in project.yaml

--- src/mappings/mappingDotExchangeRateHistory.ts
+++ src/mappings/mappingDotExchangeRateHistory.ts
@@ -4,18 +4,17 @@ import { Compact } from '@polkadot/types';
 import { DotExchangeRate } from "../types/models/DotExchangeRate";
 import { Rate } from "@parallel-finance/types/interfaces";
 
-export async function createDotExchangeRateHistory(block: SubstrateBlock): Promise<void> {
+export async function dotExchangeRateHistory(block: SubstrateBlock): Promise<void> {
     const blockNumber = (block.block.header.number as Compact<BlockNumber>).toBigInt();
 
     const record = new DotExchangeRate(blockNumber.toString());
 
-
     record.blockNumber = blockNumber;
 
-    const dotExchangeRate = (await api.query.loan.exchangeRate('DOT')) as Rate;
+    const dotExchangeRate = (await api.query.loans.exchangeRate('KSM')) as Rate;
 
     record.rate = dotExchangeRate.toString();
 
-    await record.save();
+    await record.save().catch(e => { console.log(e) });
 
 }
