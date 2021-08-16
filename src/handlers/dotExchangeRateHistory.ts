b51b62ea82618b89defc61874881b2444044fc5f
Ayush <>
2021-08-16 16:45:24 +0530

    minor refactoring

--- src/handlers/dotExchangeRateHistory.ts
+++ src/handlers/dotExchangeRateHistory.ts
@@ -2,6 +2,7 @@ import { SubstrateBlock, SubstrateEvent } from "@subql/types";
 import { BlockNumber } from "@polkadot/types/interfaces";
 import { Compact } from '@polkadot/types';
 import { DotExchangeRate } from "../types/models/DotExchangeRate";
+import { Rate } from "@parallel-finance/types/interfaces";
 
 export async function createDotExchangeRateHistory(block: SubstrateBlock): Promise<void> {
     const blockNumber = (block.block.header.number as Compact<BlockNumber>).toBigInt();
@@ -11,7 +12,7 @@ export async function createDotExchangeRateHistory(block: SubstrateBlock): Promi
 
     record.blockNumber = blockNumber;
 
-    const dotExchangeRate = (await api.query.liquidStaking.exchangeRate);
+    const dotExchangeRate = (await api.query.loan.exchangeRate('DOT')) as Rate;
 
     record.rate = dotExchangeRate.toString();
 
