e4c52f2c800fbc7a09caffdeb9331b60433c4f7c
Ayush <>
2021-08-16 07:40:02 +0530

    Added type for Dot Exchange Rate

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -2,13 +2,13 @@ import { Codec } from '@polkadot/types/types'
 
 export const TOKEN_DECIMAL = {
   DOT: 10,
+  KSM: 12,
   USDT: 6,
   xDOT: 12,
   xKSM: 12,
+  HKO: 12,
   PARA: 12,
-  KSM: 12,
-  HKO: 12
-}
+ }
 
 export function resolveToken (token: Codec) {
     const name = (token as any).asToken.toString()
---
+++ src/types/models/DotExchangeRate.ts
@@ -0,0 +1,47 @@
+// Auto-generated , DO NOT EDIT
+import {Entity} from "@subql/types";
+import assert from 'assert';
+
+
+export class DotExchangeRate implements Entity {
+
+    constructor(id: string) {
+        this.id = id;
+    }
+
+
+    public id: string;
+
+    public rate?: number;
+
+    public blockId?: string;
+
+
+    async save(): Promise<void>{
+        let id = this.id;
+        assert(id !== null, "Cannot save DotExchangeRate entity without an ID");
+        await store.set('DotExchangeRate', id.toString(), this);
+    }
+    static async remove(id:string): Promise<void>{
+        assert(id !== null, "Cannot remove DotExchangeRate entity without an ID");
+        await store.remove('DotExchangeRate', id.toString());
+    }
+
+    static async get(id:string): Promise<DotExchangeRate | undefined>{
+        assert((id !== null && id !== undefined), "Cannot get DotExchangeRate entity without an ID");
+        const record = await store.get('DotExchangeRate', id.toString());
+        if (record){
+            return DotExchangeRate.create(record);
+        }else{
+            return;
+        }
+    }
+
+
+
+    static create(record){
+        let entity = new DotExchangeRate(record.id);
+        Object.assign(entity,record);
+        return entity;
+    }
+}
